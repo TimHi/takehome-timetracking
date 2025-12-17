6812269c58bc18767218ea3ac205aebbf6fc069e
TimHi <TimHiller@live.de>
2025-12-17 01:53:15 +0100

    Add TODO for WorkDayValidationException handling

--- shared-sdk/shared/src/commonMain/kotlin/timhi/timetracker/shared_sdk/validation/WorkDayValidationException.kt
+++ shared-sdk/shared/src/commonMain/kotlin/timhi/timetracker/shared_sdk/validation/WorkDayValidationException.kt
@@ -1,5 +1,6 @@
 package timhi.timetracker.shared_sdk.validation
 
+//TODO_THL: Dont use exception, return error code
 class WorkDayValidationException(
     val error: WorkDayValidationError
 ) : IllegalArgumentException(error.name)
