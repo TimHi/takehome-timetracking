f4f2b3b44a5d6df804a50be6231f7f1e8c167fa9
TimHi <TimHiller@live.de>
2025-12-19 21:15:41 +0100

    feat: Add ID to core WorkDay domain model

--- shared-sdk/shared/src/commonMain/kotlin/timhi/timetracker/shared_sdk/model/Workday.kt
+++ shared-sdk/shared/src/commonMain/kotlin/timhi/timetracker/shared_sdk/model/Workday.kt
@@ -2,6 +2,7 @@ package timhi.timetracker.shared_sdk.model
 import kotlinx.datetime.LocalDate
 
 data class WorkDay(
+    val id: Long? = null,
     val date: LocalDate,
     val timeRanges: List<TimeRange>,
 )
