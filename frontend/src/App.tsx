9304810c7e0e2d08554f2aa3d06764b9e2923f38
TimHi <TimHiller@live.de>
2025-12-19 20:38:39 +0100

    chore(frontend): Cleanup App.tsx and update global styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,7 @@
 import './App.css';
-import { JsTimeRange, JsWorkDay, validate } from 'shared';
-import { WeeklyView } from './views/WeeklyView';
+import WeeklyView from './views/WeeklyView';
 
 function App() {
-	const wt = [
-		new JsTimeRange('2025-12-16T08:00:00Z', '2025-12-16T12:00:00Z'),
-		new JsTimeRange('2025-12-16T13:00:00Z', '2025-12-16T17:00:00Z'),
-	];
-
-	const bt = [new JsTimeRange('2025-12-16T12:00:00Z', '2025-12-16T12:30:00Z')];
-
-	const wd = new JsWorkDay('2025-12-16', wt, bt);
-
-	// --- 4️⃣ Validate ---
-	let isValid = false;
-
-	try {
-		validate(wd); // Calls Kotlin validator
-		isValid = true;
-	} catch (e) {
-		console.error('Validation failed:', e);
-	}
-
-	console.log('is Valid:', isValid);
 	return (
 		<>
 			<WeeklyView />
